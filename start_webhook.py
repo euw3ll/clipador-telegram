1475422799d9e3cdcfa72e0fbbbed53b5c0a1572
Wendrell Antoneli <w3ll@MacBook-Air-de-Wendrell.local>
2025-05-05 11:04:17 -0300

    Porta do RENDER

--- start_webhook.py
+++ start_webhook.py
@@ -3,5 +3,5 @@ import os
 from core.gateway.webhook_kirvano import app
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5100))
+    port = int(os.environ.get("PORT", 10000))  # Render define essa variável automaticamente
     app.run(host="0.0.0.0", port=port)
